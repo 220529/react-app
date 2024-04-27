6585b5346b43d47c0297dca0b76e44ed28ee2b94
fuliping <fuliping@bus365.com>
2024-04-27 17:08:51 +0800

    fix: axios

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,37 +1,69 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+import { notification } from "antd";
 
-const config = {
+const config: AxiosRequestConfig = {
   baseURL: process.env.NODE_ENV === "development" ? "" : "http://47.93.17.251:9005",
   timeout: 5000,
   withCredentials: true,
 };
 
 class Axios {
-  service: AxiosInstance;
+  private service: AxiosInstance;
+
   constructor(config: AxiosRequestConfig) {
     this.service = axios.create(config);
+    this.initInterceptors();
+  }
+
+  private initInterceptors() {
+    // 请求拦截器
     this.service.interceptors.request.use(
       config => {
+        // 在发送请求之前做些什么，比如添加公共请求头
         return config;
       },
       error => {
+        // 对请求错误做些什么
         return Promise.reject(error);
       }
     );
+
+    // 响应拦截器
     this.service.interceptors.response.use(
-      response => {
-        return response;
-      },
+      response => this.handleResponse(response),
       error => {
-        return Promise.reject(error);
+        // 对响应错误做些什么
+        if (error.response) {
+          // 服务器返回了错误响应
+          return this.handleResponse(error.response);
+        } else {
+          // 未收到服务器响应，可能是网络错误等
+          return Promise.reject(error);
+        }
       }
     );
   }
-  get(url: string, params?: object, header = {}) {
-    return this.service.get(url, { params, ...header });
+
+  private handleResponse(response: AxiosResponse) {
+    if (response.status === 200) {
+      // 如果响应正常，则返回数据部分
+      return Promise.resolve(response.data);
+    } else {
+      // 如果响应异常，则显示错误通知
+      notification.error({
+        message: "Error",
+        description: response.data.message || "未知错误",
+      });
+      return Promise.resolve(response.data);
+    }
   }
-  post(url: string, params?: object, header = {}) {
-    return this.service.post(url, params, header);
+
+  get(url: string, params?: object, headers?: object) {
+    return this.service.get(url, { params, headers });
+  }
+
+  post(url: string, data?: object, headers?: object) {
+    return this.service.post(url, data, { headers });
   }
 }
 
