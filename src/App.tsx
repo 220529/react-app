ab33e338aae35c39f023459a9193a93234a2481c
liukaixin <15713857919@163.com>
2024-08-14 17:45:01 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,10 @@
 import React, { useEffect } from "react";
-import { sa } from "@ns-library/monitor";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  useEffect(() => {
-    sa.beforeUpload = () => {
-      console.log("beforeUpload...");
-    };
-    sa.afterUpload = () => {
-      console.log("afterUpload...");
-    };
-    sa.platTrack("click", { pid: "iiii", aid: "发来的；fks" });
-  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Learn React
     </div>
   );
 }
